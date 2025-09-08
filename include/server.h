6b0360c5d6660fbfd95dd04a8bbb905c75e34ef2
bot4o <boevaleksandar@gmail.com>
2025-09-08 10:56:21 +0300

    segfualt on runtime

---
+++ include/server.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <cstdio>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <cstring>
+
+#define PORT 8080
+#define VIEWSDIR = "./public/";
+
+class Server {
+private:
+    int sockfd;
+    int clientfd;
+    bool CreateServerSocket();
+    char* GetRequest();
+    bool AnalyzeRequest(char* requestBuff);
+    bool ReturnIndex();
+    bool SendResponse(const char* responseBuff);
+public:
+    Server();
+    virtual ~Server();
+    bool Start();
+};
--- src/server.cpp
+++ src/server.cpp
@@ -1,1 +1,126 @@
+#include "../include/server.h"
+
+Server::Server() {
+    CreateServerSocket();
+}
+
+Server::~Server() {
+    int result = close(sockfd);
+    if(result == -1) {
+        std::cout << "ERROR: Couldn't close server socket";
+        return;
+    }
+
+    std::cout << "Server closed";
+}
+
+bool Server::CreateServerSocket() {
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd == -1) {
+        std::cout << "Socket creation error!" << std::endl;
+        std::cout << errno << std::endl;
+        return false;
+    }
+    struct sockaddr_in address = {
+        AF_INET,
+        htons(PORT),
+        {0} //localhost
+    };
+
+    int result = bind(sockfd, (struct sockaddr *)&address, sizeof(address));
+    if(result == -1) { 
+        std::cout << "ERROR: Binding IP to socket file descriptor" << std::endl;
+        std::cout << errno << std::endl;
+        return false;
+    }
+
+    //Setting server socket to wait for a request from client
+    result = listen(sockfd, 1);
+    if(result == -1) {
+        std::cout << "ERROR: Setting listen() on socket file descriptor failed" << std::endl;
+        std::cout << errno << std::endl;
+        return false;
+    }
+    return true;
+}
+
+char* Server::GetRequest() {
+    char* requestBuff = nullptr;
+    int valread = read(clientfd, &requestBuff, sizeof(requestBuff));
+    if(valread > 0) {
+        return requestBuff;
+    }
+    return requestBuff;
+}
+
+bool Server::AnalyzeRequest(char* requestBuff) {
+    char* method = strtok(requestBuff, " ");
+    char* requestTarget = strtok(NULL, " ");
+    char* protocol = strtok(NULL, "\r\n");
+    
+    if(strcmp(protocol, "HTTP/1.1") == 0) {
+        if(strcmp(method, "GET") == 0) {
+            if(requestTarget[0] == '/') {
+                ReturnIndex();
+            }
+        }
+    }
+
+    return true;
+}
+
+bool Server::ReturnIndex() {
+    FILE* file = fopen("./public/index.html", "r");
+    if(!file) {
+        std::cout << "ERROR: Could not open index.html\n";
+        return false;
+    }
+    char fileBuffer[2048];
+    long bytesRead = fread(fileBuffer, 1, sizeof(fileBuffer) - 1, file);
+    fileBuffer[bytesRead] = '\0';
+    fclose(file);
+
+    char responseBuff[4096];
+    responseBuff[0] = '\0';
+    strcat(responseBuff, "HTTP/1.1 200 OK\r\n");
+    strcat(responseBuff, "Content-Type: text/html\r\n");
+    strcat(responseBuff, "\r\n");
+    strcat(responseBuff, fileBuffer);
+
+    SendResponse(responseBuff);
+    return true;
+}
+
+bool Server::SendResponse(const char* responseBuff) {
+    int result = send(clientfd, responseBuff, strlen(responseBuff), 0);
+    if(result == -1) {
+        std::cout << "ERROR: Failed to send a response" << std::endl;
+        return false;
+    }
+    else {
+        result = close(clientfd);
+        if(result == -1) {
+            std::cout << "ERROR: Couldn't close client socket";
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Server::Start() {
+    std::cout << "Server listening on port " << PORT << "...\n";
+
+    for(;;) {
+        clientfd = accept(sockfd, 0 , 0);
+        if(clientfd == -1)  {
+            std::cout << "ERROR: Socket creation error!" << std::endl;
+            std::cout << errno << std::endl;
+            return false;
+        }
+
+        char* requestBuff = GetRequest();
+        std::cout << requestBuff << std::endl;
+        AnalyzeRequest(requestBuff);
+    }
+}
 
